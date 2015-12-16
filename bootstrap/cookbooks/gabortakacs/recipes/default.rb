b2088fa2a6a88a88005b79a7b3cd24cd988f8a80
Gabor Takacs <gabor.takacs@cheppers.com>
2015-12-16 23:45:48 +0100

    Nginx update to 1.9.9, HTTP2 enabled

--- bootstrap/cookbooks/gabortakacs/recipes/default.rb
+++ bootstrap/cookbooks/gabortakacs/recipes/default.rb
@@ -1,11 +1,25 @@
 nginx_conf_path = '/etc/nginx/conf.d'
 
+apt_repository 'nignx' do
+  uri 'http://nginx.org/packages/mainline/ubuntu/'
+  distribution 'trusty'
+  components ['nginx']
+  key 'http://nginx.org/keys/nginx_signing.key'
+  notifies :run, 'execute[apt-get update]', :immediately
+end
+
+package 'nginx' do
+  version "#{node[:nginx][:version]}*"
+end
+
 service 'nginx' do
   action :stop
 end
 
-file "#{nginx_conf_path}/default.conf" do
-  action :delete
+Dir.glob("#{nginx_conf_path}/*.conf") do |conf_file|
+  file conf_file do
+    action :delete
+  end
 end
 
 template "#{nginx_conf_path}/gabortakacs.conf" do
@@ -27,11 +41,10 @@ end
 
 directory '/etc/nginx/ssl'
 
-unless File.exists? '/etc/letsencrypt/live/gabortakacs.com/fullchain.pem'
-  execute 'get_ssl_cert' do
-    cwd "#{site_root}/letsencrypt"
-    command './letsencrypt-auto certonly --standalone -d gabortakacs.com --email me@gabortakacs.com --agree-tos'
-  end
+execute 'get_ssl_cert' do
+  cwd "#{site_root}/letsencrypt"
+  command './letsencrypt-auto certonly --standalone -d gabortakacs.com --email me@gabortakacs.com --agree-tos'
+  not_if "#{File.exists?('/etc/letsencrypt/live/gabortakacs.com/fullchain.pem')}"
 end
 
 service 'nginx' do
